fcf03761892eef3171f974836c644a90384a6c41
uchansansan <uchansansan@gmail.com>
2020-09-16 22:26:29 +0700

    Update solution.c

--- solution.c
+++ solution.c
@@ -13,19 +13,28 @@ int main(void) {
 	scanf("%d", &n);
 	double r = sqrt(a*a + b*b);//модуль
 	double r1 = a*a + b*b;
-  if(a>0)
-	  printf("Тригонометрическая запись числа: %lf*(cos(%lf)+isin(%lf))\n",r,atan((double)b/a), atan((double)b/a));
-  if((a<0)&&(b>0))
-    printf("Тригонометрическая запись числа: %lf*(cos(%lf)+isin(%lf))\n",r,atan((double)b/a)+M_PI, atan((double)b/a)+M_PI);
-  if((a<0)&&(b<0))
-    printf("Тригонометрическая запись числа: %lf*(cos(%lf)+isin(%lf))\n",r,atan((double)b/a)-M_PI, atan((double)b/a)-M_PI);
-	printf("Число обратное данному: %lf/%lf+i*%lf/%lf\n",a,r1,b,r1);
+  	if(a>0)
+		printf("Тригонометрическая запись числа: %lf*(cos(%lf)+isin(%lf))\n",r,atan((double)b/a), atan((double)b/a));
+	else if (a < 0){
+		if(b>=0)
+    		printf("Тригонометрическая запись числа: %lf*(cos(%lf)+isin(%lf))\n",r,atan((double)b/a)+M_PI, atan((double)b/a)+M_PI);
+    	else
+    		printf("Тригонометрическая запись числа: %lf*(cos(%lf)+isin(%lf))\n",r,atan((double)b/a)-M_PI, atan((double)b/a)-M_PI);
+	}
+	else if (a==0){
+		if(b>=0)
+    		printf("Тригонометрическая запись числа: %lf*(cos(%lf)+isin(%lf))\n",r,atan((double)b/a)+M_PI, atan((double)b/a)+M_PI);
+    	else
+    		printf("Тригонометрическая запись числа: %lf*(cos(%lf)+isin(%lf))\n",r,M_PI/2, M_PI/2);	
+	}
 
-	printf("Число в степени n: %lf*(cos(%lf)+isin(%lf))\n",pow(r,(double)n), n*acos((double)a/r), n*asin((double)b/r));
+	printf("Число обратное данному: %lf/%lf+i*%lf/%lf\n",a,r1,-b,r1);
+
+	printf("Число в степени n: %lf*(cos(%lf)+isin(%lf))\n",pow(r,(double)n), n*(atan((double)b/a)),n*(atan((double)b/a)));
 
 	printf("Все корни n-ой степени из данного числа\n");
 	for(int k=0;k<n;k++){
-		printf("%lf*(cos(%lf+2pi*%lf)+isin(%lf+2pi*%lf))\n",pow(r,1/n),acos((double)a/r)/n, (double)k/n, acos((double)a/r)/n, (double)k/n);
+		printf("%lf*(cos(%lf+2pi*%lf)+isin(%lf+2pi*%lf))\n", pow(r,1/n), atan((double)b/a)/n, (double)k/n, atan((double)b/a)/n, (double)k/n);
 
 	}
 	return 0;
