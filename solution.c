8c1ef1bc7a68abce3fc60c691ed3f3c7ff4d5c71
uchansansan <uchansansan@gmail.com>
2020-09-17 22:21:12 +0700

    Update solution.c

--- solution.c
+++ solution.c
@@ -15,27 +15,51 @@ int main(void) {
 	double r1 = a*a + b*b;
   	if(a>0)
 		printf("Тригонометрическая запись числа: %lf*(cos(%lf)+isin(%lf))\n",r,atan((double)b/a), atan((double)b/a));
+
+		printf("Число в степени n: %lf*(cos(%lf)+isin(%lf))\n",pow(r,(double)n), n*(atan((double)b/a)),n*(atan((double)b/a)));
+
+		printf("Все корни n-ой степени из данного числа\n");
+		for(int k=0;k<n;k++){
+			printf("%lf*(cos(%lf+2pi*%lf)+isin(%lf+2pi*%lf))\n", pow(r,1/n), atan((double)b/a)/n, (double)k/n, atan((double)b/a)/n, (double)k/n);
+
 	else if (a < 0){
 		if(b>=0)
     		printf("Тригонометрическая запись числа: %lf*(cos(%lf)+isin(%lf))\n",r,atan((double)b/a)+M_PI, atan((double)b/a)+M_PI);
+
+    		printf("Число в степени n: %lf*(cos(%lf)+isin(%lf))\n",pow(r,(double)n), n*((atan((double)b/a))+M_PI),n*((atan((double)b/a))+M_PI);
+
+			printf("Все корни n-ой степени из данного числа\n");
+			for(int k=0;k<n;k++){
+				printf("%lf*(cos(%lf+2pi*%lf)+isin(%lf+2pi*%lf))\n", pow(r,1/n), (atan((double)b/a)+M_PI)/n, (double)k/n, (atan((double)b/a)+M_PI)/n, (double)k/n);
     	else
     		printf("Тригонометрическая запись числа: %lf*(cos(%lf)+isin(%lf))\n",r,atan((double)b/a)-M_PI, atan((double)b/a)-M_PI);
+
+    		printf("Число в степени n: %lf*(cos(%lf)+isin(%lf))\n",pow(r,(double)n), n*((atan((double)b/a))-M_PI),n*((atan((double)b/a))-_PI);
+
+			printf("Все корни n-ой степени из данного числа\n");
+			for(int k=0;k<n;k++){
+				printf("%lf*(cos(%lf+2pi*%lf)+isin(%lf+2pi*%lf))\n", pow(r,1/n), (atan((double)b/a)-M_PI)/n, (double)k/n, (atan((double)b/a)-M_PI)/n, (double)k/n);
 	}
 	else if (a==0){
 		if(b>0)
     		printf("Тригонометрическая запись числа: %lf*(cos(%lf)+isin(%lf))\n",r,M_PI/2, M_PI/2);
+
+    		printf("Число в степени n: %lf*(cos(%lf)+isin(%lf))\n",pow(r,(double)n), n*(M_PI/2),n*(M_PI/2));
+
+			printf("Все корни n-ой степени из данного числа\n");
+			for(int k=0;k<n;k++){
+				printf("%lf*(cos(%lf+2pi*%lf)+isin(%lf+2pi*%lf))\n", pow(r,1/n), (M_PI/2)/n, (double)k/n, (M_PI/2)/n, (double)k/n);
     	else
-    		printf("Тригонометрическая запись числа: %lf*(cos(%lf)+isin(%lf))\n",r,-M_PI/2, -M_PI/2);	
+    		printf("Тригонометрическая запись числа: %lf*(cos(%lf)+isin(%lf))\n",r,-M_PI/2, -M_PI/2);
+
+    		printf("Число в степени n: %lf*(cos(%lf)+isin(%lf))\n",pow(r,(double)n), n*(-M_PI/2),n*(-M_PI/2));
+
+			printf("Все корни n-ой степени из данного числа\n");
+			for(int k=0;k<n;k++){
+				printf("%lf*(cos(%lf+2pi*%lf)+isin(%lf+2pi*%lf))\n", pow(r,1/n), (-M_PI/2)/n, (double)k/n, (-M_PI/2)/n, (double)k/n);
 	}
 
 	printf("Число обратное данному: %lf/%lf+i*%lf/%lf\n",a,r1,-b,r1);
-
-	printf("Число в степени n: %lf*(cos(%lf)+isin(%lf))\n",pow(r,(double)n), n*(atan((double)b/a)),n*(atan((double)b/a)));
-
-	printf("Все корни n-ой степени из данного числа\n");
-	for(int k=0;k<n;k++){
-		printf("%lf*(cos(%lf+2pi*%lf)+isin(%lf+2pi*%lf))\n", pow(r,1/n), atan((double)b/a)/n, (double)k/n, atan((double)b/a)/n, (double)k/n);
-
 	}
 	return 0;
 }
